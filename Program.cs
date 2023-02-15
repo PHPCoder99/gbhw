6e84b809f145f8f48237e883b28a94fe221c5319
Zo PHPCoder99 <gergenkopzoya@gmail.com>
2023-02-15 18:04:39 +0700

    solved 4 problems

--- Program.cs
+++ Program.cs
@@ -6,7 +6,52 @@ namespace gbhw
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 1...
+            // Console.Write("Enter first number: ");
+            // int num1 = Convert.ToInt32(Console.ReadLine());
+            // Console.Write("Enter second number: ");
+            // int num2 = Convert.ToInt32(Console.ReadLine());
+            // if (num1 > num2){
+            //     Console.WriteLine(num1);
+            // } else if (num1 < num2){
+            //     Console.WriteLine(num2);
+            // } else{
+            //     Console.WriteLine("Numbers are equal");
+            // }
+
+            // 2...
+            // Console.Write("Enter first number: ");
+            // int num1 = Convert.ToInt32(Console.ReadLine());
+            // Console.Write("Enter second number: ");
+            // int num2 = Convert.ToInt32(Console.ReadLine());
+            // Console.Write("Enter third number: ");
+            // int num3 = Convert.ToInt32(Console.ReadLine());
+            // if (num1 > num2 && num1 > num3){
+            //     Console.WriteLine(num1);
+            // } else if (num2 > num1 && num2 > num3){
+            //     Console.WriteLine(num2);
+            // } else if (num3 > num1 && num3 > num2){
+            //     Console.WriteLine(num3);
+            // } else{
+            //     Console.WriteLine("Numbers are equal");
+            // }
+
+            // 3...
+            // Console.Write("Enter number: ");
+            // int num = Convert.ToInt32(Console.ReadLine());
+            // if (num % 2 == 0){
+            //     Console.WriteLine("yes");
+            // } else{
+            //     Console.WriteLine("no");
+            // }
+
+            // 4...
+            // Console.Write("Enter number: ");
+            // int num = Convert.ToInt32(Console.ReadLine());
+            // for (int i = 2; i <= num; i+=2)
+            // {
+            //     Console.WriteLine(i);
+            // }
         }
     }
 }
